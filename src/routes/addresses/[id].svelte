bfdd5a8b1d4ecaac8d73cb69716a877e2fbf1df1
null <null>
2025-04-29 11:43:16 -0400

    example of issue with strict false

---
+++ src/routes/addresses/[id].svelte
@@ -0,0 +1,12 @@
+<script>
+
+  import { params } from '@roxi/routify'
+  $: id = $params.id
+
+</script>
+
+<p>
+Your address id is <b>{id}</b>
+</p>
+
+<a href="/">Home</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,26 @@
+<script>
+
+  import { goto } from '@roxi/routify'
+
+  function gotohelper() {
+    $goto('/addresses/2', {}, {strict: false});
+  }
+
+  function gotoreplace() {
+    $goto('/addresses/3', {}, {strict: false, replace: true});
+  }
+</script>
+
 <h1>Welcome to Your New Routify 3 Project!</h1>
+
+<p>
+<a href="/addresses/1">Embedded href</a>
+</p>
+
+<p>
+<button on:click={gotohelper}>goto helper</button>
+</p>
+
+<p>
+<button on:click={gotoreplace}>goto replace</button>
+</p>
